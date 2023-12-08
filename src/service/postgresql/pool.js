c9cee6ccdf77e427616d7944809a26fe905fb1fe
FanFauzi <if3309291@gmail.com>
2023-12-08 15:15:03 +0700

    adding connectionString

--- src/service/postgresql/pool.js
+++ src/service/postgresql/pool.js
@@ -1,14 +1,18 @@
 /* istanbul ignore file */
 const { Pool } = require('pg');
 
-const testConfig = {
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-};
+const connestionString = 'postgresql://datapenduduk330:CaLyxE9AKF3V@ep-gentle-heart-28962184.ap-southeast-1.aws.neon.tech/datpen?sslmode=require';
 
-const pool = new Pool(testConfig);
+// const testConfig = {
+//   host: process.env.PGHOST,
+//   port: process.env.PGPORT,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE,
+// };
+
+const pool = new Pool({
+  connectionString: connestionString,
+});
 
 module.exports = pool;
